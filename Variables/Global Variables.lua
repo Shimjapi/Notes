8f23bbdbbb2e0beef00ba425affb0c243ed9c1cb
Shimjapi <139707168+Shimjapi@users.noreply.github.com>
2025-02-09 11:57:31 -0800

    Create Global Variables.lua

---
+++ Variables/Global Variables.lua
@@ -0,0 +1,10 @@
+_G.Identifier = 'This is also a string' -- Name = Identifier
+
+Boolean = false -- Name = Boolean
+
+shared.NoValue = false -- Name = NoValue
+shared.NoValue2 -- Name = NoValue2
+
+getgenv().Synapse = game.Players.LocalPlayer
+-- Exploiting global variable
+-- Name = Synapse
